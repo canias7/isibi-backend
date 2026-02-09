928de368759bece137566648ad4165545dd634b5
marioambar <marioambar@MacBookAir.lan>
2026-02-09 11:25:04 -0500

    Fix CORS credentials for Lovable

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],   # later restrict to lovable domain
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
